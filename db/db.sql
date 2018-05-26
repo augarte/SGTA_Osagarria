1707518eb3055bbb77473060767e264735e22e23
Aimar Ugarte <ugarteaimar@gmail.com>
2018-05-26 19:39:21 +0200

    Datubasea eguneratua

--- db/db.sql
+++ db/db.sql
@@ -1,49 +1,194 @@
-CREATE TABLE Erabiltzailea(
-	nan VARCHAR(9) NOT NULL UNIQUE,
-	izena VARCHAR(100) NOT NULL, 
-	abizenak VARCHAR(100) NOT NULL, 
-	pasahitza VARCHAR(255) NOT NULL,
-	emaila VARCHAR(100) NOT NULL,
-	telefonoa INT(12) NOT NULL,
-	helbidea VARCHAR(255) NOT NULL,
-	sortze_data DATE NOT NULL, 
-	PRIMARY KEY (nan));
+DROP DATABASE `bankua`;
+CREATE DATABASE  IF NOT EXISTS `bankua` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `bankua`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bankua
+-- ------------------------------------------------------
+-- Server version 5.7.20
 
-CREATE TABLE Faboritoa(
-	id INT NOT NULL AUTO_INCREMENT, 
-	erabiltzaile_id VARCHAR(9) NOT NULL,
-	faborito_id VARCHAR(9) NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY (erabiltzaile_id) REFERENCES Erabiltzailea (nan),
-	FOREIGN KEY (faborito_id) REFERENCES Erabiltzailea (nan));
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- CREATE TABLE Kontua(
--- 	kontu_zenbakia VARCHAR(24) NOT NULL,
--- 	iban VARCHAR(4) NOT NULL,
--- 	bankua INT(4) NOT NULL,
--- 	bulegoa INT(4) NOT NULL,
--- 	kontrola INT(2) NOT NULL,
--- 	zenbakia INT(10) NOT NULL AUTO_INCREMENT,
--- 	erabiltzaile_id VARCHAR(9) NOT NULL,
--- 	PRIMARY KEY (zenbakia, kontu_zenbakia),
--- 	FOREIGN KEY (erabiltzaile_id) REFERENCES Erabiltzailea (nan));
+--
+-- Table structure for table `Accounts`
+--
 
-CREATE TABLE Kontua(
-	kontu_zenbakia VARCHAR(24) NOT NULL,
-	erabiltzaile_id VARCHAR(9) NOT NULL,
-	PRIMARY KEY (kontu_zenbakia),
-	FOREIGN KEY (erabiltzaile_id) REFERENCES Erabiltzailea (nan));
+DROP TABLE IF EXISTS `Accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Accounts` (
+  `kontu_zenbakia` varchar(24) NOT NULL,
+  `erabiltzaile_id` varchar(9) NOT NULL,
+  PRIMARY KEY (`kontu_zenbakia`),
+  KEY `erabiltzaile_id` (`erabiltzaile_id`),
+  CONSTRAINT `accounts_ibfk_1` FOREIGN KEY (`erabiltzaile_id`) REFERENCES `users` (`nan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE Transakzioa(
-	id INT NOT NULL AUTO_INCREMENT,
-	bildaltzaile_kontua VARCHAR(24) NOT NULL,
-	hartzaile_kontua VARCHAR(24) NOT NULL,
-	kopurua INT NOT NULL,
-	azalpena VARCHAR(255),
-	data DATE NOT NULL,
-	CHECK (kopurua>0),
-	PRIMARY KEY (id),
-	FOREIGN KEY (bildaltzaile_kontua) REFERENCES Kontua (kontu_zenbakia),
-	FOREIGN KEY (hartzaile_kontua) REFERENCES Kontua (kontu_zenbakia));
+--
+-- Dumping data for table `Accounts`
+--
 
+LOCK TABLES `Accounts` WRITE;
+/*!40000 ALTER TABLE `Accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Accounts` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `Favorites`
+--
+
+DROP TABLE IF EXISTS `Favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Favorites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `erabiltzaile_id` varchar(9) NOT NULL,
+  `faborito_id` varchar(9) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `erabiltzaile_id` (`erabiltzaile_id`),
+  KEY `faborito_id` (`faborito_id`),
+  CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`erabiltzaile_id`) REFERENCES `users` (`nan`),
+  CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`faborito_id`) REFERENCES `users` (`nan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Favorites`
+--
+
+LOCK TABLES `Favorites` WRITE;
+/*!40000 ALTER TABLE `Favorites` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Favorites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Transactions`
+--
+
+DROP TABLE IF EXISTS `Transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bildaltzaile_kontua` varchar(24) NOT NULL,
+  `hartzaile_kontua` varchar(24) NOT NULL,
+  `kopurua` int(11) NOT NULL,
+  `azalpena` varchar(255) DEFAULT NULL,
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bildaltzaile_kontua` (`bildaltzaile_kontua`),
+  KEY `hartzaile_kontua` (`hartzaile_kontua`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`bildaltzaile_kontua`) REFERENCES `accounts` (`kontu_zenbakia`),
+  CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`hartzaile_kontua`) REFERENCES `accounts` (`kontu_zenbakia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Transactions`
+--
+
+LOCK TABLES `Transactions` WRITE;
+/*!40000 ALTER TABLE `Transactions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `nan` varchar(9) NOT NULL,
+  `izena` varchar(100) NOT NULL,
+  `abizenak` varchar(100) NOT NULL,
+  `pasahitza` varchar(255) NOT NULL,
+  `emaila` varchar(100) NOT NULL,
+  `telefonoa` int(12) NOT NULL,
+  `helbidea` varchar(255) NOT NULL,
+  `sortze_data` date NOT NULL,
+  PRIMARY KEY (`nan`),
+  UNIQUE KEY `nan` (`nan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('12345678A','user','userA','pass','user@gmail.com',688611111,'Bilbo','2018-05-26');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ar_internal_metadata`
+--
+
+DROP TABLE IF EXISTS `ar_internal_metadata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ar_internal_metadata` (
+  `key` varchar(255) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ar_internal_metadata`
+--
+
+LOCK TABLES `ar_internal_metadata` WRITE;
+/*!40000 ALTER TABLE `ar_internal_metadata` DISABLE KEYS */;
+INSERT INTO `ar_internal_metadata` VALUES ('environment','development','2018-05-25 21:31:19','2018-05-25 21:31:19');
+/*!40000 ALTER TABLE `ar_internal_metadata` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20180523181902');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-26 19:37:29
