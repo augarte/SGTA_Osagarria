4ff057612f0d79167ba1c31efd6318a4097ff9a9
Aimar Ugarte <ugarteaimar@gmail.com>
2018-05-29 00:18:35 +0200

    errore tipografikoa

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,6 @@ class UserController < ApplicationController
   	@erabiltzailea = User.find(id)
   	p @erabiltzailea
   	@kontuak = Account.find_by_sql("SELECT  `accounts`.* FROM `accounts` WHERE `accounts`.`erabiltzaile_id` = '#{id}'")
-  	@transakzioak = Transaction.find_by_bildaltzaile_kontua(kontua)
+  	@transakzioak = Transaction.find_by_bidaltzaile_kontua(kontua)
   end
 end
