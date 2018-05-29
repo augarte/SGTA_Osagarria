47f1a7448f0da999d28c310d12d6beb49e3bab11
Aimar Ugarte <ugarteaimar@gmail.com>
2018-05-29 23:55:56 +0200

    datu basean datuak automatikoki sartu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Erabiltzaileak
+@user = User.new do |u|
+              u.nan = "12345678A",
+              u.izena = "user",
+              u.abizenak = "userA",
+              u.pasahitza = "pass",
+              u.emaila = "user@gmail.com"
+              u.telefonoa = "688611111"
+              u.helbidea = "Bilbo"
+              u.sortze_data = Time.now.strftime("%Y-%m-%d")
+        end
+@user.save()
 
+@user = User.new do |u|
+              u.nan = "01234567B",
+              u.izena = "user2",
+              u.abizenak = "userB",
+              u.pasahitza = "pass",
+              u.emaila = "user2@gmail.com"
+              u.telefonoa = "688622222"
+              u.helbidea = "Donostia"
+              u.sortze_data = Time.now.strftime("%Y-%m-%d")
+        end
+@user.save()
 
-# User = User.create([{ izena: 'User' }, { name: 'Lord of the Rings' }])
-# Character.create(name: 'Luke', movie: movies.first)
+#Kontuak
+@account = Account.new do |a|
+				a.kontu_zenbakia = "0123456789012",
+              	a.ebiltzaile_id = "01234567B",
+              	a.kopurua = "5",
+             	a.sortze_data = Time.now.strftime("%Y-%m-%d")
+			end
+@account.save()
+
+@account = Account.new do |a|
+				a.kontu_zenbakia = "1234567890123",
+              	a.ebiltzaile_id = "12345678A",
+              	a.kopurua = "100",
+             	a.sortze_data = Time.now.strftime("%Y-%m-%d")
+			end
+@account.save()
+
+@account = Account.new do |a|
+				a.kontu_zenbakia = "1234567891234",
+              	a.ebiltzaile_id = "12345678A",
+              	a.kopurua = "30",
+             	a.sortze_data = Time.now.strftime("%Y-%m-%d")
+			end
+@account.save()
